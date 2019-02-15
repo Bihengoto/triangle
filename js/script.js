b2455fd04a14d0c84cd876bf09f6910637ac26db
Mahugu <isaacbihengoto@gmail.com>
2019-02-15 17:14:20 +0300

    added the condition on the script file in the js folder

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-document.getElementById("myForm").addEventListner("identity", triangle);
+//document.getElementById("myForm").addEventListner("identity", triangle);
 
-function triangle(e) {
+function triangle() {
   var inputOne = parseInt(getElementById("one").value);
   var inputTwo = parseInt(getElementById("two").value);
   var inputThree = parseInt(getElementById("three").value);
+
+  //var inputArray = [];
+  //var allValues = inputArray.push(inputOne, inputTwo, inputThree);
+  if (
+    inputOne + inputTwo > inputThree ||
+    inputTwo + inputThree > inputOne ||
+    inputOne + inputThree > inputTwo
+  ) {
+  }
 
   //e.preventDefault();
 }
