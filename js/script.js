03806e0d34b8d43657f800d336f7eb6d4acae9f0
Mahugu <isaacbihengoto@gmail.com>
2019-02-15 16:20:31 +0300

    modified the index file by inserting a few tags and attribute also added the script file with a number of elements

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("myForm").addEventListner("identity", triangle);
+
+function triangle(e) {
+  var inputOne = parseInt(getElementById("one").value);
+  var inputTwo = parseInt(getElementById("two").value);
+  var inputThree = parseInt(getElementById("three").value);
+
+  //e.preventDefault();
+}
